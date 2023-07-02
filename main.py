b356e3f46a1435546692b15b1dd73c6dcc770dae
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-07-02 08:46:19 +0600

    feat: add today filter

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from collections import Counter
 import pandas as pd
 import requests
 import streamlit as st
+from datetime import datetime
 
 st.title("Unity Dashboard")
 name = st.text_input("Enter your name", max_chars=50)
@@ -12,7 +13,14 @@ if name:
     r = requests.get(url)
 
     data = r.json()["distractions"]
-    distracted_hours = [distraction["time"].split("T")[1][0:2] for distraction in data]
+    # sample 2023-07-01T16:54:36.920Z
+    now = datetime.now()
+    distracted_hours = [
+        distraction["time"].split("T")[1][0:2]
+        for distraction in data
+        # Showing for today
+        if distraction["time"][0:10] == str(now)[0:10]
+    ]
 
     distraction_count = Counter(distracted_hours)
 
